a2b9fe0795e4679c71448b51c6ca463c1d001485
hanhaibo <hbhan@tendyron.com>
2022-05-10 15:08:12 +0800

    Flutter系列之九：自定义bottomNavigationBar

--- lib/pages/example/ttf/MyIcons.dart
+++ lib/pages/example/ttf/MyIcons.dart
@@ -2,25 +2,30 @@ import 'package:flutter/material.dart';
 
 class MyIcons{
   static const IconData home= const IconData(
-      0xe604,
+      0xe60a,
       fontFamily: "MyIcons",
       matchTextDirection: true
   );
 
   static const IconData life= const IconData(
-      0xe604,
+      0xe60e,
       fontFamily: "MyIcons",
       matchTextDirection: true
   );
 
   static const IconData money= const IconData(
-      0xe604,
+      0xe60b,
       fontFamily: "MyIcons",
       matchTextDirection: true
   );
 
   static const IconData mine= const IconData(
-      0xe604,
+      0xe60d,
+      fontFamily: "MyIcons",
+      matchTextDirection: true
+  );
+  static const IconData card= const IconData(
+      0xe60c,
       fontFamily: "MyIcons",
       matchTextDirection: true
   );
--- lib/pages/example/ttf/layout_example.dart
+++ lib/pages/example/ttf/layout_example.dart
@@ -8,7 +8,11 @@ void main() {
 class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MainPage());
+    return MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.green,
+        ),
+        home: MainPage());
   }
 }
 
@@ -18,6 +22,14 @@ class MainPage extends StatefulWidget {
 }
 
 class _HomePageState extends State<MainPage> {
+  late int currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,19 +38,53 @@ class _HomePageState extends State<MainPage> {
         padding: const EdgeInsets.all(100),
         child: Row(
           children: [
-            Icon(MyIcons.home,size: 100,),
+            Icon(
+              MyIcons.home,
+              size: 100,
+              color: Colors.green,
+            ),
             Text("This is a ttf!"),
           ],
         ),
       ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //     items: [
-      //       BottomNavigationBarItem(icon: Icon(MyIcons.home),label: "首页"),
-      //       BottomNavigationBarItem(icon: Icon(MyIcons.life),label: "生活"),
-      //       BottomNavigationBarItem(icon: Icon(MyIcons.money),label: "金融"),
-      //       BottomNavigationBarItem(icon: Icon(MyIcons.mine),label: "我的"),
-      //     ]
-      // ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (index) {
+            _changePage(index);
+          },
+          selectedItemColor: Colors.green,
+          unselectedItemColor: Colors.black38,
+          // backgroundColor: Colors.white,
+          iconSize: 40,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(MyIcons.home),
+                activeIcon: Icon(MyIcons.home),
+                label: "首页"),
+            BottomNavigationBarItem(
+                icon: Icon(MyIcons.life),
+                activeIcon: Icon(MyIcons.life),
+                label: "生活"),
+            BottomNavigationBarItem(
+                icon: Icon(MyIcons.money),
+                activeIcon: Icon(MyIcons.money),
+                label: "理财"),
+            BottomNavigationBarItem(
+                icon: Icon(MyIcons.mine),
+                activeIcon: Icon(MyIcons.mine),
+                label: "我的"),
+          ]),
     );
+  }
+
+  /*切换页面*/
+  void _changePage(int index) {
+    /*如果点击的导航项不是当前项  切换 */
+    if (index != currentIndex) {
+      setState(() {
+        currentIndex = index;
+      });
+    }
   }
 }
